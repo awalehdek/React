0a5714060aec3e2eae00386961093c3da2556c81
awalehdek <awalehdek@gmail.com>
2025-09-04 22:53:03 -0700

    cursor button fix

--- Transcript-viewer/transcrip-viewer/src/components/Header.jsx
+++ Transcript-viewer/transcrip-viewer/src/components/Header.jsx
@@ -5,10 +5,11 @@ export default function Header() {
     <header className='flex items-center justify-between gap-4 p-4'>
             <h1>Transcript<span className='text-purple-400'> Viewer</span></h1>
 
-            <button className='flex items-center gap-2 '>
+            <button className="flex items-center gap-2 cursor-pointer">
               <p>New</p>
-              <i class="fa-solid fa-plus"></i>
+              <i className="fa-solid fa-plus"></i>
             </button>
+
           </header>
   )
 }
