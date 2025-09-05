e01632f99f7637304fe12ebaff9fe2fe1037e122
awalehdek <awalehdek@gmail.com>
2025-09-04 23:15:12 -0700

    add poppen font

--- Transcript-viewer/transcrip-viewer/src/components/Homepage.jsx
+++ Transcript-viewer/transcrip-viewer/src/components/Homepage.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Homepage() {
   return (
     <main className='flex-1 p-4 flex flex-col justify-center '>
-        dsddsdss
+        <h1 className="font-extrabold tracking-tight text-5xl sm:text-6xl md:text-7xl">
+          Transcript <span className="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">Viewer</span>
+        </h1>
+
     </main>
   )
 }
