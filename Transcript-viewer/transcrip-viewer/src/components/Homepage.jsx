ec087b8c67d89fcdabfc9b4a3c4840c9ae3f3c2a
awalehdek <awalehdek@gmail.com>
2025-09-06 18:27:58 -0700

    Record, translate, edit

--- Transcript-viewer/transcrip-viewer/src/components/Homepage.jsx
+++ Transcript-viewer/transcrip-viewer/src/components/Homepage.jsx
@@ -6,7 +6,9 @@ export default function Homepage() {
         <h1 className="font-extrabold tracking-tight text-5xl sm:text-6xl md:text-7xl">
           Transcript <span className="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">Viewer</span>
         </h1>
-
+        <h3  className='font-medium'>Record<span className='text-blue-400'>&rarr;</span> Translate
+          <span className='text-blue-400'>&rarr;</span> View & Edit 
+        </h3>
     </main>
   )
 }
