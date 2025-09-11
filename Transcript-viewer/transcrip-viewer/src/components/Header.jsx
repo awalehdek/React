b6c162dca6b14b7f2b6eea8b4f26b1e79686d27d
awalehdek <awalehdek@gmail.com>
2025-09-10 22:44:47 -0700

    create specialbtn effect and added into header

--- Transcript-viewer/transcrip-viewer/src/components/Header.jsx
+++ Transcript-viewer/transcrip-viewer/src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className='flex items-center justify-between gap-4 p-4'>
             <h1>Transcript<span className='text-purple-400'> Viewer</span></h1>
 
-            <button className="flex items-center gap-2 cursor-pointer">
+            <button className="flex items-center gap-2 cursor-pointer specialbtn font-bold py-2 px-4 rounded">
               <p>New</p>
               <i className="fa-solid fa-plus"></i>
             </button>
